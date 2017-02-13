0236a15f77763f99ea886b87cd7b174a43a3a9bf
Alex-Tsvetanov <alex_tsvetanov_2002@abv.bg>
2017-02-13 23:17:34 +0200

    first commit

--- news.php
+++ news.php
@@ -62,7 +62,6 @@
 				display:inline-block;
 				margin-right:10px;
 				width: 45%;
-				height: 540px;
 			}
 			#all article>aside {
 				padding-top: 240px;
@@ -73,7 +72,6 @@
 				display:inline-block;
 				text-align: left;
 				width: 45%;
-				height: 540px;
 			}
 			
 			article.hideMe:after {
@@ -100,7 +98,7 @@
 	</head>
 
 	<body>
-		 <nav class="navbar navbar-default">
+		 <nav class="navbar navbar-inverse">
 			<div class="container-fluid">
 				<div class="navbar-header">
 
@@ -127,25 +125,28 @@
 			<!-- Indicators -->
 			<ol class="carousel-indicators">
 			</ol>
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox"> </div>
-			<!-- Left and right controls -->
-			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="false"></span> </a>
-			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="false"></span> </a>
+			<div class="carousel-inner" role="listbox">
+			</div>
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 		<div id="all"> </div>
 		<div class="container">
-		<div id="row" class="row">
-			<div id="joke" class="col-md-10">
-				<h1>Joke:</h1><br>
-				{{joke}}
+			<div id="row" class="row">
+				<div id="joke" class="col-md-10">
+					<h1>Joke:</h1><br>
+					{{joke}}
+				</div>
 			</div>
 		</div>
-		</div>
-		<ul class="pager">
-			<li class="previous"><a href="" id="prev">Previous</a></li>
-			<li class="next"><a href="" id="next">Next</a></li>
-		</ul>
+		<hr>
+		<footer><center><p>Copyrights © Tech Education++ 2016-2017</p></center></footer>
 		<script>
 			let slides = $(".carousel-inner")[0];
 			let all = $("#all");
