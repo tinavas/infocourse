2309f008acde5e2949800de6497cc683f855ad8a
Alex-Tsvetanov <alex_tsvetanov_2002@abv.bg>
2017-02-14 11:54:06 +0200

    first commit

--- lib/get_seminars.php
+++ lib/get_seminars.php
@@ -12,7 +12,7 @@
 	{
 		while($row = $result->fetch_assoc()) 
 		{
-			echo "<div class=\"col-md-3\" style=\"background-color: #fff; margin: 10px; border-radius: 25px; border: 3px solid orange;/* padding: 1px 10px 1px;*/\"> <h2>{$row["Name"]}</h2> <p><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-secondary\">View details »</a></p> </div>";
+			echo "<div class=\"col-md-3\" style=\"background-color: #fff; margin: 10px; border-radius: 25px; border: 3px solid orange;/* padding: 1px 10px 1px;*/\"> <h2>{$row["Name"]}</h2><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-raised active\">View video »</a><a href=\"#\" role=\"button\" class=\"btn btn-raised btn-success\">Do exam »</a> </div>";
 		}
 	}
 ?>
